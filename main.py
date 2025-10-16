6371158a212068e1d0cf2cc5c4fce49fee3b9806
DanYiFan <148186042+DanYiFan@users.noreply.github.com>
2025-10-16 12:42:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_access_token():
     # appSecret
     app_secret = config["app_secret"]
     post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                .format(app_id, app_secret))
+                。format(app_id, app_secret))
     try:
         access_token = get(post_url).json()['access_token']
     except KeyError:
@@ -181,7 +181,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+            #birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)   //////////////////////////////////////////需要显示生日倒计时，去掉注释////////////////////////////////////////////
+            birthday_data = "还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
