fae756cc2e37ae018ce0c55e85a2c10c1c139089
DanYiFan <148186042+DanYiFan@users.noreply.github.com>
2025-10-16 12:50:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_access_token():
     # appSecret
     app_secret = config["app_secret"]
     post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                。format(app_id, app_secret))
+                .format(app_id, app_secret))
     try:
         access_token = get(post_url).json()['access_token']
     except KeyError:
@@ -181,12 +181,11 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)  
-            #birthday_data = "距离{}回家过年还有{}天".format(value["name"], birth_day)
+            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'，
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
