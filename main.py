953ce40f1e9c969125e99ff50191c182a2a88835
DanYiFan <148186042+DanYiFan@users.noreply.github.com>
2025-10-16 13:05:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def get_access_token():
     app_id = config["app_id"]
     # appSecret
     app_secret = config["app_secret"]
-    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                。format(app_id, app_secret))
+    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}".format(app_id, app_secret))
     try:
         access_token = get(post_url).json()['access_token']
     except KeyError:
